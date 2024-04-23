c96cc19b243bb871a6b7d760b0829a78f6aec6c4
maria-osan <mariaosan30@yahoo.com>
2024-04-23 17:33:53 +0300

    Lab SGBD

---
+++ An2/Semestrul 2/Sisteme de Gestiune a Bazelor de Date/Lab2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridParent = new System.Windows.Forms.DataGridView();
+            this.dataGridChild = new System.Windows.Forms.DataGridView();
+            this.labelDestinatii = new System.Windows.Forms.Label();
+            this.labelPacheteTuristice = new System.Windows.Forms.Label();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridParent
+            // 
+            this.dataGridParent.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridParent.GridColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridParent.Location = new System.Drawing.Point(67, 55);
+            this.dataGridParent.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridParent.Name = "dataGridParent";
+            this.dataGridParent.RowHeadersWidth = 80;
+            this.dataGridParent.Size = new System.Drawing.Size(800, 185);
+            this.dataGridParent.TabIndex = 0;
+            // 
+            // dataGridChild
+            // 
+            this.dataGridChild.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridChild.GridColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridChild.Location = new System.Drawing.Point(67, 326);
+            this.dataGridChild.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridChild.Name = "dataGridChild";
+            this.dataGridChild.RowHeadersWidth = 100;
+            this.dataGridChild.Size = new System.Drawing.Size(800, 98);
+            this.dataGridChild.TabIndex = 1;
+            // 
+            // labelDestinatii
+            // 
+            this.labelDestinatii.AutoSize = true;
+            this.labelDestinatii.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDestinatii.Location = new System.Drawing.Point(67, 25);
+            this.labelDestinatii.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDestinatii.Name = "labelDestinatii";
+            this.labelDestinatii.Size = new System.Drawing.Size(107, 21);
+            this.labelDestinatii.TabIndex = 2;
+            this.labelDestinatii.Text = "Parent table:";
+            // 
+            // labelPacheteTuristice
+            // 
+            this.labelPacheteTuristice.AutoSize = true;
+            this.labelPacheteTuristice.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPacheteTuristice.Location = new System.Drawing.Point(67, 295);
+            this.labelPacheteTuristice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPacheteTuristice.Name = "labelPacheteTuristice";
+            this.labelPacheteTuristice.Size = new System.Drawing.Size(97, 21);
+            this.labelPacheteTuristice.TabIndex = 3;
+            this.labelPacheteTuristice.Text = "Child table:";
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReload.Location = new System.Drawing.Point(734, 590);
+            this.buttonReload.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(133, 31);
+            this.buttonReload.TabIndex = 10;
+            this.buttonReload.Text = "Reload data";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(523, 464);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 31);
+            this.buttonAdd.TabIndex = 11;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(644, 464);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(100, 31);
+            this.buttonUpdate.TabIndex = 12;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(766, 464);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 31);
+            this.buttonDelete.TabIndex = 13;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(67, 464);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 157);
+            this.panel1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 660);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.labelPacheteTuristice);
+            this.Controls.Add(this.labelDestinatii);
+            this.Controls.Add(this.dataGridChild);
+            this.Controls.Add(this.dataGridParent);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridParent;
+        private System.Windows.Forms.DataGridView dataGridChild;
+        private System.Windows.Forms.Label labelDestinatii;
+        private System.Windows.Forms.Label labelPacheteTuristice;
+        private System.Windows.Forms.Button buttonReload;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ An2/Semestrul 2/Sisteme de Gestiune a Bazelor de Date/Lab2/Form1.cs
@@ -0,0 +1,281 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Data.Common;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        SqlDataAdapter daParent = new SqlDataAdapter();
+        SqlDataAdapter daChild = new SqlDataAdapter();
+
+        string ParentTableName = ConfigurationManager.AppSettings["ParentTableName"];
+        string ChildTableName = ConfigurationManager.AppSettings["ChildTableName"];
+
+        string ChildColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+        List<string> ColumnNamesList = new List<string>(ConfigurationManager.AppSettings["ChildColumnNames"].Split(','));
+        string ColumnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+            SqlConnection cs = new SqlConnection(con);
+
+            string selectFromParent = ConfigurationSettings.AppSettings["selectFromParent"];
+            daParent.SelectCommand = new SqlCommand(selectFromParent, cs);
+            ds.Clear();
+            daParent.Fill(ds, ParentTableName);
+            dataGridParent.DataSource = ds.Tables[ParentTableName];
+
+            dataGridParent.SelectionChanged += DataGridParent_SelectionChanged;
+            dataGridChild.SelectionChanged += dataGridChild_SelectionChanged;
+
+            int labelY = 5; // Initial Y position for labels
+            int textFieldY = 5; // Initial Y position for text fields
+
+            foreach (string columnName in ColumnNamesList.Skip(1).Take(ColumnNamesList.Count - 2))
+            {
+                Label label = new Label();
+                label.Text = columnName + ":";
+                label.AutoSize = true;
+                label.Location = new Point(0, labelY);
+                label.Font = new Font("Microsoft PhagsPa", 11.25f, FontStyle.Regular);
+
+                TextBox textField = new TextBox();
+                textField.Name = columnName;
+                textField.Location = new Point(110, textFieldY);
+                textField.Width = 200;
+
+                panel1.Controls.Add(label);
+                panel1.Controls.Add(textField);
+
+                labelY += 30;
+                textFieldY += 30;
+            }
+        }
+
+        private void DataGridParent_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridParent.SelectedRows.Count > 0)
+            {
+                DataRowView selectedRow = dataGridParent.SelectedRows[0].DataBoundItem as DataRowView;
+                int parentId = Convert.ToInt32(selectedRow[0]);
+
+                string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+                SqlConnection cs = new SqlConnection(con);
+
+                string selectFromChild = ConfigurationSettings.AppSettings["selectFromChild"];
+
+                SqlCommand cmd = new SqlCommand(selectFromChild, cs);
+                cmd.Parameters.AddWithValue("@ID", parentId);
+
+                if (ds.Tables.Contains(ChildTableName))
+                {
+                    ds.Tables[ChildTableName].Clear();
+                }
+
+                daChild.SelectCommand = cmd;
+                daChild.Fill(ds, ChildTableName);
+                dataGridChild.DataSource = ds.Tables[ChildTableName];
+            }
+        }
+
+        private void dataGridChild_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridChild.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridChild.SelectedRows[0];
+
+                foreach (Control control in panel1.Controls)
+                {
+                    if (control is TextBox)
+                    {
+                        string columnName = control.Name;
+
+                        if (selectedRow.Cells[columnName] != null)
+                        {
+                            control.Text = selectedRow.Cells[columnName].Value.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonReload_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+            SqlConnection cs = new SqlConnection(con);
+
+            // Reload parent data
+            string selectFromParent = ConfigurationSettings.AppSettings["selectFromParent"];
+            daParent.SelectCommand = new SqlCommand(selectFromParent, cs);
+            ds.Clear();
+            daParent.Fill(ds, ParentTableName);
+
+            // Reload child data if there's a parent row selected
+            if (dataGridParent.SelectedRows.Count > 0)
+            {
+                DataRowView selectedRow = dataGridParent.SelectedRows[0].DataBoundItem as DataRowView;
+                int parentId = Convert.ToInt32(selectedRow[0]);
+
+                string selectFromChild = ConfigurationSettings.AppSettings["selectFromChild"];
+
+                SqlCommand cmd = new SqlCommand(selectFromChild, cs);
+                cmd.Parameters.AddWithValue("@ID", parentId);
+
+                ds.Tables[ChildTableName].Clear();
+                daChild.SelectCommand = cmd;
+                daChild.Fill(ds, ChildTableName);
+            }
+
+            dataGridParent.DataSource = ds.Tables[ParentTableName];
+            dataGridChild.DataSource = ds.Tables[ChildTableName];
+
+            foreach (Control control in panel1.Controls)
+            {
+                if (control is TextBox)
+                {
+                    control.Text = string.Empty;
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+                SqlConnection cs = new SqlConnection(con);
+                cs.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO " + ChildTableName + " (" + ChildColumnNames + ") VALUES (" + ColumnNamesInsertParameters + ")", cs);
+
+                string maxIdCommand = ConfigurationSettings.AppSettings["maxIdCommand"];
+                SqlCommand getMaxIdCommand = new SqlCommand(maxIdCommand, cs);
+                int maxId = Convert.ToInt32(getMaxIdCommand.ExecuteScalar());
+                int id = maxId + 1;
+
+                DataRowView selectedParentRow = dataGridParent.SelectedRows[0].DataBoundItem as DataRowView;
+                int parentId = Convert.ToInt32(selectedParentRow[0]);
+
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@parentId", parentId);
+
+                int pos = 1;
+                List<string> ColumnNamesInsertParametersList = new List<string>(ConfigurationManager.AppSettings["ColumnNamesInsertParameters"].Split(','));
+                foreach (string column in ColumnNamesList.Skip(1).Take(ColumnNamesList.Count - 2))
+                {
+                    TextBox textBox = (TextBox)panel1.Controls[column];
+                    cmd.Parameters.AddWithValue(ColumnNamesInsertParametersList[pos], textBox.Text);
+                    pos++;
+                }
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+
+                // Refresh the child DataGridView
+                if (ds.Tables.Contains(ChildTableName))
+                {
+                    ds.Tables[ChildTableName].Clear();
+                }
+                daChild.Fill(ds, ChildTableName);
+                dataGridChild.DataSource = ds.Tables[ChildTableName];
+
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+                SqlConnection cs = new SqlConnection(con);
+                cs.Open();
+
+                string update = ConfigurationSettings.AppSettings["UpdateQuery"];
+                SqlCommand cmd = new SqlCommand(update, cs);
+
+                DataGridViewRow selectedRow = dataGridChild.SelectedRows[0];
+                int id = Convert.ToInt32(selectedRow.Cells[0].Value);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int pos = 1;
+                List<string> ColumnNamesInsertParametersList = new List<string>(ConfigurationManager.AppSettings["ColumnNamesInsertParameters"].Split(','));
+                foreach (string column in ColumnNamesList.Skip(1).Take(ColumnNamesList.Count - 2))
+                {
+                    TextBox textBox = (TextBox)panel1.Controls[column];
+                    cmd.Parameters.AddWithValue(ColumnNamesInsertParametersList[pos], textBox.Text);
+                    pos++;
+                }
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+
+                // Refresh the child DataGridView
+                if (ds.Tables.Contains(ChildTableName))
+                {
+                    ds.Tables[ChildTableName].Clear();
+                }
+                daChild.Fill(ds, ChildTableName);
+                dataGridChild.DataSource = ds.Tables[ChildTableName];
+
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+                SqlConnection cs = new SqlConnection(con);
+                cs.Open();
+
+                string delete = ConfigurationSettings.AppSettings["DeleteQuery"];
+                SqlCommand cmd = new SqlCommand(delete, cs);
+
+                DataGridViewRow selectedRow = dataGridChild.SelectedRows[0];
+                int id = Convert.ToInt32(selectedRow.Cells[0].Value);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+
+                // Refresh the child DataGridView
+                if (ds.Tables.Contains(ChildTableName))
+                {
+                    ds.Tables[ChildTableName].Clear();
+                }
+                daChild.Fill(ds, ChildTableName);
+                dataGridChild.DataSource = ds.Tables[ChildTableName];
+
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message);
+            }
+        }
+    }
+}
